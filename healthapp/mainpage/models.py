b6d0a9185ea75f9c182f150bca46fea54a86e57d
Alex <climberfields@gmail.com>
2018-11-29 14:31:27 -0600

    made migrations and established user model

--- healthapp/mainpage/models.py
+++ healthapp/mainpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.IntegerField(max_length=100)
+    water = models.IntegerField(max_length=100)
+    alcohol = models.IntegerField(max_length=100)
+    caffeine = models.IntegerField(max_length=100)
+
+    def __str__(self):
+        return self.name
